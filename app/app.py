bd66eca78c4fec1e681834c3ff61437aa0743051
daichi <daichi970918@gmail.com>
2020-11-12 14:09:17 +0900

    2日目完

--- app/app.py
+++ app/app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
     name = request.args.get("name")
-    return render_template("index.html",name=name)
+    okyo = ["色不異空","空不異色","色即是空","空即是色"]
+    return render_template("index.html",name=name,okyo=okyo)
 
 
 if __name__ == "__main__":
