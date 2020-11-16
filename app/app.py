db29a531b9bf5b2bd421a3b0685858f40cd0b4b0
daichi <daichi970918@gmail.com>
2020-11-16 15:49:47 +0900

    3日目終了

--- app/app.py
+++ app/app.py
@@ -9,7 +9,13 @@ def index():
     name = request.args.get("name")
     okyo = ["色不異空","空不異色","色即是空","空即是色"]
     return render_template("index.html",name=name,okyo=okyo)
-
+#以下を追加
+@app.route("/index",methods=["post"])
+def post():
+    name = request.form["name"]
+    okyo = ["色不異空", "空不異色", "色即是空", "空即是色"]
+    return render_template("index.html", name=name, okyo=okyo)
+#追加終わり
 
 if __name__ == "__main__":
     app.run(debug=True)
