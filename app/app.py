689679dd5923d344eeb3f80414faf4628a0c28ab
daichi <daichi970918@gmail.com>
2020-11-16 14:31:22 +0900

    1日目終了

--- app/app.py
+++ app/app.py
@@ -1,15 +1,22 @@
-from flask import Flask,render_template,request
+#Flaskとrender_template（HTMLを表示させるための関数）をインポート
+from flask import Flask,render_template
 
+#Flaskオブジェクトの生成
 app = Flask(__name__)
 
 
+#「/」へアクセスがあった場合に、"Hello World"の文字列を返す
 @app.route("/")
+def hello():
+    return "Hello World"
+
+
+#「/index」へアクセスがあった場合に、「index.html」を返す
 @app.route("/index")
 def index():
-    name = request.args.get("name")
-    okyo = ["色不異空","空不異色","色即是空","空即是色"]
-    return render_template("index.html",name=name,okyo=okyo)
+    return render_template("index.html")
 
 
+#おまじない
 if __name__ == "__main__":
     app.run(debug=True)
