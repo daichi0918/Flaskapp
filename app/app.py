55f4b5d1b95824882e89b739059bb6e2c2459cc3
daichi <daichi970918@gmail.com>
2020-11-16 13:41:50 +0900

    DBの初期化とsqliteできた。

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask,render_template,request
+#下記のインポート文を追加
+from models.models import OnegaiContent
 
 app = Flask(__name__)
 
@@ -7,13 +9,20 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
     name = request.args.get("name")
-    okyo = ["色不異空","空不異色","色即是空","空即是色"]
-    return render_template("index.html",name=name,okyo=okyo)
+    #以下を変更
+    all_onegai = OnegaiContent.query.all()
+    return render_template("index.html",name=name,all_onegai=all_onegai)
+    #変更終わり
+
+
 @app.route("/index",methods=["post"])
 def post():
     name = request.form["name"]
-    okyo = ["色不異空", "空不異色", "色即是空", "空即是色"]
-    return render_template("index.html", name=name, okyo=okyo)
+    #ここも変更
+    all_onegai = OnegaiContent.query.all()
+    return render_template("index.html", name=name, all_onegai=all_onegai)
+    #変更終わり
+
 
 if __name__ == "__main__":
     app.run(debug=True)
