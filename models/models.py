d9a1a75d399f8a6f3f505b120d5045bbd23e77ef
daichi <daichi970918@gmail.com>
2020-11-18 08:20:13 +0900

    6日目完全終了

--- models/models.py
+++ models/models.py
@@ -17,3 +17,18 @@ class OnegaiContent(Base):
 
     def __repr__(self):
         return '<Title %r>' % (self.title)
+
+#以下を追加
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(128), unique=True)
+    hashed_password = Column(String(128))
+
+    def __init__(self, user_name=None, hashed_password=None):
+        self.user_name = user_name
+        self.hashed_password = hashed_password
+
+    def __repr__(self):
+        return '<Name %r>' % (self.user_name)
+#追加終わり
